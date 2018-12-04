df2dc2609386753a6c2c3c9289704086988e6dff
fabiano <fabianofelix3@gmail.com>
2018-12-04 11:26:08 -0200

    Hashcode and equals

--- model/entities/Client.java
+++ model/entities/Client.java
@@ -4,9 +4,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
-	
+
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	private String name;
 	private String email;
 	private Date birthDate;
@@ -44,5 +44,36 @@ public class Client {
 	@Override
 	public String toString() {
 		return name + " (" + sdf.format(birthDate) + ") - " + email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 }
