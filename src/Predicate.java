7379840298069090bb1f509ff0c2d1d59f91ce97
fabiano <fabianofelix3@gmail.com>
2018-12-09 20:23:50 -0200

    Predicate2

--- src/Predicate.java
+++ src/Predicate.java
@@ -17,7 +17,7 @@ public class Predicate {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("Hd Case", 80.90));
 
-		list.removeIf(new ProductPredicate());
+		list.removeIf(Product::staticProductPredicate);
 
 		for (Product p : list) {
 			System.out.println(p);
