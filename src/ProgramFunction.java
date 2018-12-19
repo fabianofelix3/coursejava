3e01655e6600ecd429207efcbcbd196111cd5c08
fabiano <fabianofelix3@gmail.com>
2018-12-19 17:23:49 -0200

    Function4 - Solution with declared lambda expression

--- src/ProgramFunction.java
+++ src/ProgramFunction.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class ProgramFunction {
 
@@ -18,7 +18,9 @@ public class ProgramFunction {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("Hd Case", 80.90));
 
-		List<String> names = list.stream().map(Product::noStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 
 		names.forEach(System.out::println);
 	}
